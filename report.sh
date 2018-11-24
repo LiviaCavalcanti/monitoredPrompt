4054d032c0e05ebac6ca1e78ff9372cb6a1d2f76
docinho <livia.juliao@ccc.ufcg.edu.br>
2018-11-23 23:48:55 -0300

    selecionando os tempos do usuario por mes

--- report.sh
+++ report.sh
@@ -1,2 +1,4 @@
 # PREVIA DO HISTOGRAMA
 awk -v d=$(date +%m) -F- '$2== d' log.txt | awk '{ print $7 }' | sort|uniq -c
+
+# awk -v d=$(date +%m) -F- '$2== d' log.txt | awk '{ print $13" "$15 }'  falta fazer a soma dos tempos de sys e de user
