51e29deb5c1509d673e76aa0639f03ef221a09a8
antonioPedroNeto <antonio.pedro.neto@ccc.ufcg.edu.br>
2018-11-26 15:19:25 -0300

    Adicionado filtro de parametro na impressão do relatório

--- report.sh
+++ report.sh
@@ -1,4 +1,10 @@
 printf "Histograma\n"
 awk -v d=$(date +%m) -F- '$2== d' log.txt | awk '{ print $7 }' | sort|uniq -c
+aux=`awk -v d=$1 -F- '$2== d' log.txt | wc -l`
 printf "\n"
-awk -v d=$(date +%m) -F- '$2== d' log.txt | awk '{ user+=$13; sys+=$15 } END { print "Tempo gasto por user: " user  ". Tempo gasto por sys: " sys }' 
+if [ $aux -eq 0 ] 
+then
+	echo "Durante o mês $1, para o usuário $USER, nenhum registro foi armazenado"
+else
+	awk -v d=$1 -F- '$2== d' log.txt | awk '{ user+=$13; sys+=$15 } END { print "Tempo gasto por user: " user  ". Tempo gasto por sys: " sys }' 
+fi 
